db4308ea0bfe614f9fca2379b19c9a0106447809
AnnaKurniaty <Annakuykuy25@gmail.com>
2022-09-28 13:46:37 +0700

    drag

--- Assets/js/maps.js
+++ Assets/js/maps.js
@@ -1,80 +1,36 @@
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
-
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
-const container = document.querySelector('.container')
-const img = document.querySelector('svg')
-
-let zoom = 1
-container.addEventListener('wheel', e => {
-    img.style.transformOrigin = `${e.offsetX}px ${e.offsetY}px`
-
-    zoom += e.deltaY * -0.01
-    zoom = Math.min(Math.max(1, zoom), 5)
-
-    if (zoom == 1) {
-        img.style.left = '0px'
-        img.style.top = '0px'
-    }
-
-    img.style.transform = `scale(${zoom})`
-})
-
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
-
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
-
-let clicked = false
-let xAxis;
-let x;
-let yAxis;
-let y;
-
-container.addEventListener('mouseup', () => container.style.cursor = 'auto')
-
-container.addEventListener('mousedown', e => {
-    clicked = true;
-    xAxis = e.offsetX - img.offsetLeft;
-    yAxis = e.offsetY - img.offsetTop;
-
-    container.style.cursor = 'grabbing'
-})
-
-window.addEventListener('mouseup', () => clicked = false)
-
-container.addEventListener('mousemove', e => {
-    if (!clicked) return
-    e.preventDefault()
-
-    x = e.offsetX
-    y = e.offsetY
-
-    img.style.left = `${x - xAxis}px`
-    img.style.top = `${y - yAxis}px`
-
-    checkSize()
-})
-
-function checkSize () {
-    let containerOut = container.getBoundingClientRect()
-    let imgIn = img.getBoundingClientRect()
-
-    if (parseInt(img.style.left) > 0) {
-        img.style.left = '0px'
-    } else if (imgIn.right < containerOut.right) {
-        img.style.left = `-${imgIn.width - containerOut.width}px`
-    }
-    if (parseInt(img.style.top) > 0) {
-        img.style.top = '0px'
-    } else if (imgIn.bottom < containerOut.bottom) {
-        img.style.top = `-${imgIn.height - containerOut.height}px`
-    }
-}
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
-
-// SUBSCRIBE ==> https://www.youtube.com/channel/UCzWS-AiirxpTDq_AGSg9Fhg
-// Join Facebook Group ==> https://www.facebook.com/groups/html.css.javascript.albtechhh
+var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom = document.getElementById("zoom");
+      function setTransform() {
+        zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+      }
+      zoom.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+      }
+      zoom.onmouseup = function (e) {
+        panning = false;
+      }
+      zoom.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+          return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+      }
+      zoom.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+          ys = (e.clientY - pointY) / scale,
+          delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+        setTransform();
+      }
